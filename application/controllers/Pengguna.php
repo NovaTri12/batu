d6f05fe330adbe16f7673a9b57037b76afe26ea1
Agus Wibowo <aguswibowo.amd@gmail.com>
2018-03-05 09:03:08 +0700

    DATABASE CONTOH

---
+++ application/controllers/Pengguna.php
@@ -0,0 +1,19 @@
+<?php
+class Pengguna extends CI_CONTROLLER{
+	function __construct(){
+		parent::__construct();
+	}
+	function index(){
+	//daftar list pengguna
+		if($this->session->userdata('username',TRUE) && $this->session->userdata('level',TRUE)){
+
+			
+			
+		}else{
+			redirect('auth/login','refresh');
+		}
+	}
+	function detil(){
+
+	}
+}
